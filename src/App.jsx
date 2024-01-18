4ecb1c1c5c35b2a0eda707e5bf5266bb0fa33437
Mohammed Shafeek <mail2shafeek.sa@gmai.com>
2024-01-18 18:52:03 +0400

    Add tailwind css setup.

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,13 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+export default function App() {
+    getData();
 }
 
-export default App
+async function getData() {
+    fetch('https://fakestoreapi.com/products/categories')
+            .then(res=>res.json())
+        .then(json => console.log(json))
+    
+    fetch('https://fakestoreapi.com/products/category/jewelery')
+        .then(res => res.json())
+        .then(json => console.log(json));
+}
